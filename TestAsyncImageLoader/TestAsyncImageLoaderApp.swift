c1063d57375119cdc4686059f2ab89e6ea85daf1
Alex Yehorov <al.yehorov@gmail.com>
2024-07-28 13:55:07 +0100

    Initial Commit

---
+++ TestAsyncImageLoader/TestAsyncImageLoaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAsyncImageLoaderApp.swift
+//  TestAsyncImageLoader
+//
+//  Created by Alex Yehorov on 7/28/24.
+//
+
+import SwiftUI
+
+@main
+struct TestAsyncImageLoaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
