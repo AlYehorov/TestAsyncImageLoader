60ff164a7c22e8a9ef0ccf87b4ec1db43a8c6dd4
Alex Yehorov <al.yehorov@gmail.com>
2024-07-28 13:59:41 +0100

    Added test version of UI to test library

--- TestAsyncImageLoader/ContentView.swift
+++ TestAsyncImageLoader/ContentView.swift
@@ -6,16 +6,63 @@
 //
 
 import SwiftUI
+import Foundation
+import AsyncImageLoader
+
+struct ImageItem: Identifiable, Codable {
+    let id: Int
+    let imageUrl: String
+}
 
 struct ContentView: View {
+    @State var imageList: [ImageItem] = []
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button {
+            CacheManager.shared.invalidateCache()
+        } label: {
+            Text("Invalidate image cache")
+        }
+        
+        List(imageList) { item in
+            LazyVStack(alignment: .leading, spacing: 10) {
+                if let url = URL(string: "\(item.imageUrl)?size=200") {
+                    AsyncImageView(url: url, placeholder: UIImage(systemName: "photo") ?? UIImage())
+                        .clipShape(RoundedRectangle(cornerRadius: 16.0, style: .circular))
+                } else {
+                    Image(systemName: "exclamationmark.triangle")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 200)
+                        .foregroundColor(.red)
+                    Text("Invalid URL")
+                        .foregroundColor(.red)
+                }
+            }
         }
         .padding()
+        .onAppear {
+            Task {
+                await loadData()
+            }
+        }
+    }
+    
+    private func loadData() async {
+        do {
+            imageList = try await fetchImageList()
+        } catch {
+            debugPrint("something wrong")
+        }
+    }
+    
+    func fetchImageList() async throws -> [ImageItem] {
+        guard let url = URL(string: "https://zipoapps-storage-test.nyc3.digitaloceanspaces.com/image_list.json") else {
+            throw URLError(.badURL)
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let imageList = try JSONDecoder().decode([ImageItem].self, from: data)
+        return imageList
     }
 }
 
